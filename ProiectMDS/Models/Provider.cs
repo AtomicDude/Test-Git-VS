705eed90a0ad702582356841a6e88fa07aeb1293
TOMA VASILESCU <toma.vasilescu@s.unibuc.ro>
2021-03-07 19:02:26 +0200

    test

---
+++ ProiectMDS/Models/Provider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectMDS.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; }
+        public string Nume { get; set; }
+        public List <Studio> Studio { get; set; }
+    }
+}
---
+++ ProiectMDS/Models/SongAlbum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectMDS.Models
+{
+    public class SongAlbum
+    {
+        public int Id {get; set; }
+        public int songId {get; set; }
+        public int AlbumId {get; set; }
+        public virtual Song Song {get; set; }
+        public virtual Album Album {get; set; }
+    }
+}
